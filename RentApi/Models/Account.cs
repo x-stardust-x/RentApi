04858c70c402d68955fad2fdd9010083b8cae71d
x-stardust-x <s8899512345@gmail.com>
2026-05-07 15:17:32 +0800

    增加生日和年齡

--- RentApi/Models/Account.cs
+++ RentApi/Models/Account.cs
@@ -1,10 +1,17 @@
-﻿namespace RentApi.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentApi.Models {
     public class Account {
         public int Id { get; set; }
-        public string? Username { get; set; }
-        public string? Pwd { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Pwd { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
         public Identity Identity { get; set; }
-        public bool IsDelete { get; set; }
+        public bool Status { get; set; } = false;
+        public bool IsDelete { get; set; } = false;
+        public DateTime? LastLoginAt { get; set; }
     }
     public enum Identity {
         young = 0,
--- RentApi/Models/Admin.cs
+++ RentApi/Models/Admin.cs
@@ -3,7 +3,7 @@
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Pwd { get; set; }
-        public string? Name { get; set; }
+        public string? Email { get; set; }
         public string? Phone { get; set; }
     }
 }
