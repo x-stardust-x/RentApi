e3280853a74176523b8777aa37ba6d25bdb6adfe
xin26q <qq36148775@gmail.com>
2026-05-08 17:38:21 +0800

    新增房屋和工具/技能DTO內容

--- RentApi/Models/DTO/Match_HouseDto.cs
+++ RentApi/Models/DTO/Match_HouseDto.cs
@@ -2,18 +2,25 @@
 {
     public class Match_HouseDto
     {
-        #region 租賃物
+        public string ProductType { get; } = "House";
+
+        #region 房屋
 
         public string? Name { get; set; } // 名稱
-        public string? Url { get; set; } // 縮圖
+        public string? Url { get; set; } // 縮圖路徑
         public int? RentPrice { get; set; } // 價格
         public string? Address { get; set; } // 地址
 
-        // =============== 標籤群組 ===============
+        // =============== 生活習館規範 標籤群組 ===============
 
+        public int? Id { get; set; }
+        public int HouseId { get; set; } // 房屋編號
+        public int SleepTime { get; set; } // 熄燈時間
+        public int WakeTime { get; set; } // 起床時間
+        public int CleanLevel { get; set; } // 需達到整潔程度
+        public int NoiseTolerance { get; set; } // 噪音程度
         public bool? Pet { get; set; } // 是否可養寵物
         public bool? Smoke { get; set; } // 是否禁菸
-        public int? NoiseTolerance { get; set; } // 噪音程度
 
         // =======================================
 
@@ -22,19 +29,12 @@
         #endregion
 
 
+        #region 側邊篩選器
 
-        #region 篩選器
-
-        // 產品類型
-        // 地點
-        // 價格區間
-        // 生活習慣規範( 出租人規定 )
-
-
+        public int? DistrictId { get; set; } // 地點
 
 
         #endregion
-
 
     }
 }
--- RentApi/Models/DTO/Match_ProductDto.cs
+++ RentApi/Models/DTO/Match_ProductDto.cs
@@ -2,6 +2,19 @@
 {
     public class Match_ProductDto
     {
+        public string ProductType { get; } = "Skill/Tool";
 
+        #region 技能 / 工具
+
+        public string? Name { get; set; } // 技能名稱 / 工具名稱
+        public string? Url { get; set; } // 縮圖路徑
+        public int? RentPrice { get; set; } // 價格
+        public string? Address { get; set; } // 地址
+
+
+
+        public string? Description { get; set; } // 簡介
+
+        #endregion
     }
 }
