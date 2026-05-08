bf2ae3f5dac844cb54e3a759960f6cf612762503
lai88820 <lai88820@gmail.com>
2026-05-08 15:12:23 +0800

    新增HouseImage.cs controller與RentHouse.cs Controller

---
+++ RentApi/Models/DTO/AddHouseImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoLiving.models
+{
+    public class AddHouseImageDto
+    {
+        public int HouseId { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
