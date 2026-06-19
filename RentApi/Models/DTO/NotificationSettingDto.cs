9d9318b0f39c9a6500f87d176d5fb549307be2b5
x-stardust-x <s8899512345@gmail.com>
2026-06-19 18:43:16 +0800

    add Setting col

---
+++ RentApi/Models/DTO/NotificationSettingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace RentApi.Models.DTO {
+    public class NotificationSettingDto {
+        public bool phone { get; set; }
+        public bool email { get; set; }
+        public bool line { get; set; }
+    }
+}
